78add57981d026539713209e0c15b39e32b408fa
pablos <pablo.schrut@ngahr.com>
2019-08-05 14:20:52 +0200

    back end separation

--- src/app/api/models/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-    name: String,
-    tech_name: String
-});
-
-module.exports = mongoose.model('Categories', schema);
--- src/app/api/models/recipe.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const recipies = mongoose.Schema({
-    name: String
-});
-
-module.exports = mongoose.model('Recipies', recipies);
